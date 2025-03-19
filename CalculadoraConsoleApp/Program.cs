d73a08b09463686206c5000e906acff8441ac9dc
comaiivilvert <comaiivilvert@gmail.com>
2025-03-18 21:00:29 -0300

    Calculadora atualizada para envio da Aula

--- CalculadoraConsoleApp/Program.cs
+++ CalculadoraConsoleApp/Program.cs
@@ -15,7 +15,8 @@ namespace CalculadoraConsoleApp
             //Deve permitir realizar operações com numeros decimais (quebrados) - OK
             //Apresentar o resultado formatado - OK
             //
-
+            string[] operacoesRealizadas = new string[100];
+            int contadorHistorico = 0;
 
             while (true)
             {
@@ -31,6 +32,7 @@ namespace CalculadoraConsoleApp
                 Console.WriteLine("3 - Dividir");
                 Console.WriteLine("4 - Multiplicar");
                 Console.WriteLine("5 - Tabuada");
+                Console.WriteLine("6 - Historico de operações");
                 Console.WriteLine("S - Sair");
                 string opcao = Console.ReadLine();
                 bool divisaoZerada = false;
@@ -45,6 +47,7 @@ namespace CalculadoraConsoleApp
                 {
                     Console.WriteLine("Qual numero da Tabuada?");
                     int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+                    
                     for (int contador=1;contador <=10; contador++)
                     {
                         string linhaDaTabuada = numeroTabuada + " X " + contador + " = " + numeroTabuada*contador;
@@ -57,15 +60,29 @@ namespace CalculadoraConsoleApp
 
                 }
 
-                Console.Write("Digita o Primeiro número: ");
+                else if (opcao == "6")
+                {
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("Histórico de Operações");
+                    Console.WriteLine("-------------------------");
+
+                    for (int i=0; i<operacoesRealizadas.Length; i++)
+                    {
+                        if (operacoesRealizadas[i] != null)
+                            Console.WriteLine(operacoesRealizadas[i]);
+                    }
+
+                    Console.ReadLine();
+                    continue;
+                }
+
+                    Console.Write("Digita o Primeiro número: ");
                 decimal primeiroNumero = Convert.ToDecimal(Console.ReadLine());
                 
 
                 Console.Write("Digita o Segundo número: ");
                 decimal segundoNumero = Convert.ToDecimal(Console.ReadLine());
                 
-
-
                 decimal resultado = 0;
                 // estrutura de decisao
 
@@ -73,14 +90,12 @@ namespace CalculadoraConsoleApp
                 {
 
                     resultado = primeiroNumero + segundoNumero;
-                    Console.Write("O Resultado da soma é: ", resultado);
-                    Console.WriteLine(resultado.ToString("F2"));
+                    operacoesRealizadas[contadorHistorico] = $"{primeiroNumero} + {segundoNumero} = {resultado}";
                 }
                 else if (opcao == "2")
                 {
                     resultado = primeiroNumero - segundoNumero;
-                    Console.Write("O Resultado da subtração é: ", resultado);
-                    Console.WriteLine(resultado.ToString("F2"));
+                    operacoesRealizadas[contadorHistorico] = $"{primeiroNumero} - {segundoNumero} = {resultado}";
 
                 }
                 else if (opcao == "3")
@@ -89,24 +104,32 @@ namespace CalculadoraConsoleApp
                     {
                         Console.WriteLine("Impossivel divisão po ZERO!");
                         divisaoZerada = true;
+
                     }
                     else if(divisaoZerada == false)
                     {
                         resultado = primeiroNumero / segundoNumero;
-                        Console.Write("O Resultado da Divisão é: ", resultado);
-                        Console.WriteLine(resultado.ToString("F2"));
+                        operacoesRealizadas[contadorHistorico] = $"{primeiroNumero} / {segundoNumero} = {resultado}";
                     }
                 }
                 else if (opcao == "4")
                 {
                     resultado = primeiroNumero * segundoNumero;
-                    Console.Write("O Resultado da Multiplicação é: ", resultado);
-                    Console.WriteLine(resultado.ToString("F2"));
+                    operacoesRealizadas[contadorHistorico] = $"{primeiroNumero} - {segundoNumero} = {resultado}";
 
                 }
+                contadorHistorico++;
 
-                
-                Console.ReadLine();
+                Console.WriteLine("-----------------------");
+                Console.WriteLine($"RESULTADO: {resultado}");
+                Console.WriteLine("-----------------------");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine();
+
+                if (opcaoContinuar != "S")
+                    break;
+
             }
             
 
